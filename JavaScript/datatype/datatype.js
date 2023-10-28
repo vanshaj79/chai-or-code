c26e12d366d89da460612396cd3f79527970aa42
vanshaj <vanshajgangwar88@gmail.com>
2023-10-28 12:48:10 +0530

    done with data types and some conversions

---
+++ JavaScript/datatype/datatype.js
@@ -0,0 +1,16 @@
+const accountId = 12344
+let accountEmail = "vanshaj"
+var accountPassword = "7985"
+//accountCity = "jaipur"
+
+// accountId = 2 //Not allowed
+
+accountEmail = "hdfc"
+accountPassword = "122333" 
+//accountCity = "bengaluru"
+
+//prefer not to use var because of issue in block scope and functional scope
+
+console.log(accountId)
+console.table([accountId,accountEmail, accountPassword])
+
---
+++ JavaScript/datatype/datatype2.js
@@ -0,0 +1,13 @@
+"use strict"; 
+//it is used to treat all JS code as newer version
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>  
+// symbol
+
+//object
+console.log(typeof"vanshaj");
