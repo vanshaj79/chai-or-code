864e88f29071e3a45279db79b13b62043f3262e7
vanshaj <vanshajgangwar88@gmail.com>
2023-10-29 10:56:36 +0530

    completed conversions

--- JavaScript/datatype/conversionoperation.js
+++ JavaScript/datatype/conversionoperation.js
@@ -22,3 +22,24 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber)
+
+// ***************** Operations ***************
+let value = 3 
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);//power function
+console.log(2/2);
+console.log(2%2);
+
+let str1 = "hello"
+let str2 = " vanshaj"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1"+2);
+console.log("1"+2+2);
+console.log(1+2+"2")
